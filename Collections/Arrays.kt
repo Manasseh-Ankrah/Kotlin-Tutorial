023e40261495f59ccfad76d6ece41eb5fe70d576
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-02-17 13:15:09 +0000

    Practiced Array Collections

--- Collections/Arrays.kt
+++ Collections/Arrays.kt
@@ -1,5 +1,31 @@
 package Collections
 
 fun main () {
-    println("Hello")
+    var arr = arrayListOf<String>("Java","Kotlin","PHP")
+    var arraySize= arr.size
+    var first= arr.get(0)
+    var last= arr.get(arr.size - 1)
+    var setnew= arr.set(0,"JS")
+
+
+
+    println("Array Values =>> $arr")
+    println("Array Size/Length =>> $arraySize")
+    println("Array first value =>> $first")
+    println("Array LAST value =>> $last")
+    println("Array setnew value =>> $setnew")
+
+   // Looping through array
+//    for (lang in arr) {
+//        println("Array Single value =>> $lang")
+//    }
+
+   // Normal fir loop
+//
+//    for (val lang = 0; lang < arr.size; lang++ ){
+//        println("Array Single value =>> $lang")
+//    }
+
+
+
 }
