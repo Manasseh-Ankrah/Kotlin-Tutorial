12ae7ba0f3b9e28587de53813105b3b2bace79bd
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-02-17 14:49:55 +0000

    Practiced Mutable and Immutable List Collections

---
+++ Collections/Lists.kt
@@ -0,0 +1,50 @@
+package Collections
+
+
+
+ fun immutableList () {
+     val lists = listOf<String>("Rugby","Basketball","Soccer","Cricket")
+     var arraySize= lists.size
+     var first= lists.get(0)
+     var last= lists.get(lists.size - 1)
+     var setnew= lists.lastIndexOf("Cricket")
+
+
+     println("Lists =>> $lists")
+     println("arraySize =>> $arraySize")
+     println("first =>> $first")
+     println("last =>> $last")
+     println("setnew =>> $setnew")
+ }
+
+
+
+fun mutableList () {
+    val lists = mutableListOf<String>("Rugby","Basketball","Soccer","Cricket")
+    var arraySize= lists.size
+    var first= lists.get(0)
+    var last= lists.get(lists.size - 1)
+    var setnew= lists.lastIndexOf("Cricket")
+
+    println("Lists =>> $lists")
+    println("arraySize =>> $arraySize")
+    println("first =>> $first")
+    println("last =>> $last")
+    println("setnew =>> $setnew")
+    lists.add("Football")
+    println("Lists =>> $lists")
+    lists.remove("Football")
+    lists.removeAt(0)
+
+    println("Lists =>> $lists")
+
+
+}
+
+
+
+fun main () {
+//    immutableList()
+    mutableList()
+}
+
